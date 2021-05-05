d70412f7e4411d005349051f8506e013d4db1aa2
Joseph Cappadona <josephcappadona27@gmail.com>
2021-05-05 00:03:17 +0000

    reduce nonsense

--- website/js/analysis.jsx
+++ website/js/analysis.jsx
@@ -10,35 +10,8 @@ import { TopLines } from '../js/TopLines.jsx';
 import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles({
     root: {
-        minWidth: 600,
+      
     },
-    loadPGN: {
-
-    },
-    findPosition: {
-
-    },
-    topLineAnalyze: {
-
-    },
-    lineScore: {
-        width: 100,
-        height: 100,
-        fontSize: 20,
-    },
-    topLineMove: {
-        width: 100,
-        height: 100,
-        margin: 10,
-    },
-    topLineMoveText: {
-        fontSize: 16,
-        margin: 'auto',
-    },
-    topLine: {
-        height: 100,
-        margin: 15,
-    }
   });
 
 function getRandomInt(max) {
@@ -49,48 +22,10 @@ function AnalysisApp(props) {
     const classes = useStyles();
     const [sessionID, setSessionID] = React.useState(String(Date.now() + getRandomInt(10**12)));
 
-    const [PGN, setPGN] = React.useState("");
-    const [PGNLoaded, setPGNLoaded] = React.useState(false);
-
-    const [moves, setMoves] = React.useState([]);
-
-
-    var onPGNLoad = function(data) {
-        setPGN(data.PGN);
-        setPGNLoaded(true);
-    };
-
-    var onBoardChange = function(newMoves) {
-        setMoves(newMoves);
-    }
-
     return (
         <div className={classes.root}>
-            <h2>Chess Position Analysis</h2>
-
-            <h3>Step 1 - Load PGN</h3>
-            <Upload
-                sessionID={sessionID}
-                className={classes.loadPGN}
-                onLoad={onPGNLoad}
-            />
-            
-            <h3>Step 2 - Find a position</h3>
-            {PGNLoaded ? <Board
-                className={classes.findPosition}
-                sessionID={sessionID}
-                PGN={PGN}
-                onChange={onBoardChange}
-            /> : <div>Not ready</div>}
-
-
-            <h3>Step 3 - Analyze</h3>
-            {/* TOP LINES */}
-            {PGNLoaded ? <TopLines 
-                className={classes.topLineAnalyze}
-                sessionID={sessionID}
-                moves={moves}
-            /> : <div>STILL not ready</div>}
+            <h1>Chess Analysis</h1>
+            stuff goes here
         </div>
     );
 }
--- website/server.py
+++ website/server.py
@@ -21,18 +21,7 @@ def index():
 @app.route('/analysis')
 def detr():
     return render_template('analysis.html')
-
-
-class NpEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return float(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        else:
-            return super(NpEncoder, self).default(obj)
+    
 
 @app.route('/api/analysis', methods=['POST'])
 def api_analysis():
@@ -40,6 +29,7 @@ def api_analysis():
 
     if 'sessionID' not in data or 'action' not in data:
         error_code = 401
+
     else:
         error_code = 201
         session_id = data['sessionID']
@@ -49,77 +39,19 @@ def api_analysis():
             app_cache[session_id] = {}
 
         if action == 'load':
-            pgn = app_cache[session_id]['PGN'] = data['PGN']
-            pgn_filename = data['uploadedPGNFilename']
-            if (pgn_hash := hash(pgn)) != 0:
-                if not Path(pgn_filename).exists():
-                    with open(pgn_filename, 'w+t') as pgn_file:
-                        pgn_file.write(pgn)
-                games_gen = games_generator_from_file(pgn_filename)
-                games = app_cache[session_id]['games'] = list(games_gen)
-                trie = app_cache[session_id]['trie'] = make_game_trie(games)
-                ret = {'message': f'{len(games)} games loaded'}
-            else:
-                ret = {'message': f'Provided PGN empty or invalid'}
-                error_code = 201
+            ret = {}
         
         elif action == 'get-cached-pgn':
-            cached_pgn_filename = data['cachedPGNFilename']
-            games_gen = games_generator_from_file(cached_pgn_filename)
-            games = app_cache[session_id]['games'] = list(games_gen)
-            trie = app_cache[session_id]['trie'] = make_game_trie(games)
-            ret = {'message': f'{len(games)} games loaded'}
+            ret = {}
         
         elif action == 'get-moves':
-            trie = app_cache[session_id].get('trie', {})
-            if not trie:
-                games = app_cache[session_id].get('games', [])
-                if not games:
-                    ret = {'message': 'No PGN loaded'}
-                    return ret, error_code
-                else:
-                    trie = app_cache[session_id]['trie'] = make_game_trie(games)
-            moves = data['moves']
-            line_trie = get_sub_trie(trie, moves)
-            counts = {m: count_trie(line_trie[m]) for m in line_trie}
-            next_moves_zipped = sorted(counts.items(), key=lambda x: x[1], reverse=True)
-            next_moves, next_move_counts = list(zip(*next_moves_zipped))
-            total_count = sum(list(counts.values()))
-
-            if None in next_moves:
-                cur_games = line_trie[None]
-                next_moves = tuple(x for x in next_moves if x is not None)
-            else:
-                cur_games = []
-
-            ret = {
-                'message': f'{len(next_moves)} moves ({next_moves}) from prefix {moves} totaling {total_count} games' + \
-                            (f' (this includes {len(cur_games)} concluding here)' if len(cur_games) else ''),
-                'nextMoves': next_moves,
-                'curGames': cur_games,
-            }
+            ret = {}
 
         elif action == 'top-lines':
-            trie = app_cache[session_id].get('trie', {})
-            M = data.get('quantity', 5)
-            D = data.get('depth', 5)
-            moves = data.get('moves', [])
-            line_trie = get_sub_trie(trie, moves)
-            top_lines = get_top_lines(line_trie, max_depth=D)[:M]
-            top_lines_df = pd.DataFrame(line._asdict() for line in top_lines)
-            accum_prob = sum(l.score for l in top_lines)
-            data = []
-            message = f"{M} lines make up {accum_prob*100:.1f}% of the lines starting from {moves}"
-            for line in top_lines:
-                data.append([np.round(line.score, 5), list(zip(line.moves, np.round(line.freqs, 3).tolist(), line.counts))])
-            ret = {'message': message, 'topLines': data}
-            print('data', data)
+            ret = {}
         
         elif action == 'get-cached-pgn-ids':
-            from glob2 import glob
-            pgn_filenames = glob('*.pgn')
-            message = f"{len(pgn_filenames)} to choose from"
-            ret = {'message': message, 'PGNFilenames': pgn_filenames}
+            ret = {}
 
         elif action == 'find-tactics':
             ret = {}
